ba95182b3844e98860138db3d8eeb392463d7ee7
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2023-11-18 11:17:02 -0300

    feat: create first prompt

--- src/gpt.ts
+++ src/gpt.ts
@@ -1,17 +1,22 @@
 import OpenAI from 'openai';
 
+const systemPromptFile = Bun.file(`${import.meta.dir}/prompts/system.txt`, {
+  type: 'text/plain;charset=utf-8',
+});
+const systemPrompt = await systemPromptFile.text();
+
 export const openai = new OpenAI();
 
 export const chat = async (message: string) => {
   const completion = await openai.chat.completions.create({
     model: 'gpt-4-1106-preview',
     messages: [
-      { role: 'system', content: 'You are a helpful assistant' },
+      { role: 'system', content: systemPrompt },
       { role: 'user', content: message },
     ],
   });
 
-  console.log(completion.choices);
+  console.log(completion);
 
   return completion.choices[0].message;
 };
