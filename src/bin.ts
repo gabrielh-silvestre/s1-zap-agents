738db2da3cfb67ef2abcb6b24e3d38c1e4dc6d0f
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2023-11-21 00:15:36 -0300

    fix: adjust circular dependency on handlers
    - add function example into bin.ts

--- src/bin.ts
+++ src/bin.ts
@@ -1,14 +1,7 @@
 import { Client, Events, LocalAuth } from 'whatsapp-web.js';
 
-import {
-  startAgent,
-  AgentOpenAI,
-  AudioHandler,
-  SpeechHandler,
-  TextHandler,
-  TranscribeHandler,
-  defaultHandlers,
-} from '.';
+import { startAgent, AgentOpenAI, defaultHandlers } from '.';
+import { AgentFunction } from './openai/function';
 
 const AGENT_ID = process.env.AGENT_ID as string;
 if (!AGENT_ID) throw new Error('$AGENT_ID is required');
@@ -23,11 +16,46 @@ export const client = new Client({
   authStrategy: new LocalAuth(),
 });
 
+// Example Function
+export class RandomNumber extends AgentFunction {
+  constructor() {
+    super('random-number', 'Generate a random number between the given range');
+  }
+
+  async execute(args: object): Promise<any> {
+    const { min, max } = args as any;
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+}
+
 startAgent(client, {
   route: [
     {
       event: Events.MESSAGE_CREATE,
-      handlers: defaultHandlers(new AgentOpenAI(AGENT_ID)),
+      handlers: defaultHandlers(
+        new AgentOpenAI(AGENT_ID, [new RandomNumber()])
+      ),
     },
   ],
 });
+
+/*
+{
+  "name": "random-number",
+  "description": "Generate a random number between the given range",
+  "parameters": {
+    "type": "object",
+    "properties": {
+      "min": {
+        "type": "number",
+        "description": "The minimum number to generate"
+      },
+      "max": {
+        "type": "number",
+        "description": "The maximum number to generate"
+      }
+    },
+  },
+  "required": ["min", "max"],
+}
+*/
